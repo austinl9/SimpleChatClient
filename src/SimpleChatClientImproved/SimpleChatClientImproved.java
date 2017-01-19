b883b8c5675aa93bb7af2ab865823e78d6e96b38
austinl9 <acm.liu11@gmail.com>
2017-01-19 06:57:33 -0800

    finished the client need to work on the server

--- src/SimpleChatClientImproved/SimpleChatClientImproved.java
+++ src/SimpleChatClientImproved/SimpleChatClientImproved.java
@@ -18,73 +18,97 @@ import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 
 public class SimpleChatClientImproved {
-	
+
 	JTextArea incoming;
 	JTextField outgoing;
 	BufferedReader reader;
 	PrintWriter writer;
 	Socket sock;
-	
-	public void go(){
+
+	public void go() {
 		JFrame frame = new JFrame("Simple Chat Client");
 		JPanel mainPanel = new JPanel();
-		
-		//incoming chat information - can't edit with a scroll bar
-		incoming = new JTextArea(15,50);
+
+		// incoming chat information - can't edit with a scroll bar
+		incoming = new JTextArea(15, 50);
 		incoming.setLineWrap(true);
 		incoming.setWrapStyleWord(true);
 		incoming.setEditable(false);
-		
-		//scroll bar info
+
+		// scroll bar info
 		JScrollPane qScroller = new JScrollPane(incoming);
 		qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		
-		//outgoing text field takes 20 chars
+
+		// outgoing text field takes 20 chars
 		outgoing = new JTextField(20);
 		JButton sendButton = new JButton("Send");
 		sendButton.addActionListener(new SendButtonListener());
-		
-		//add all the info into the main panel
+
+		// add all the info into the main panel
 		mainPanel.add(qScroller);
 		mainPanel.add(outgoing);
 		mainPanel.add(sendButton);
 		setUpNetworking();
-		
-		//give the thread a job from incoming Readers
+
+		// give the thread a job from incoming Readers
 		Thread readerThread = new Thread(new IncomingReader());
 		readerThread.start();
-		
-		//set the size of the content
+
+		// set the size of the content
 		frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
-		frame.setSize(400,500);
+		frame.setSize(400, 500);
 		frame.setVisible(true);
 	}
-	
-	//handle the sockets here
+
+	// handle the sockets here
 	private void setUpNetworking() {
-		// TODO Auto-generated method stub
-		try{
+		try {
+			// setup a socket to the server
 			sock = new Socket("127.0.0.1", 5000);
+
+			// flow for reader client -> buffered reader -> inputstream reader
 			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
 			reader = new BufferedReader(streamReader);
+
+			// flow for writer client -> printwriter ->outputstream -> server
 			writer = new PrintWriter(sock.getOutputStream());
 			System.out.println("networking established");
-		} catch(IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
-	public class SendButtonListener implements ActionListener{
+	public class SendButtonListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			
+			try {
+				// fill the print writer
+				writer.println(outgoing.getText());
+				writer.flush();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+
+			outgoing.setText("");
+			outgoing.requestFocus();
 		}
-		
+	}
+
+	public class IncomingReader implements Runnable {
+		public void run() {
+			String message;
+			try {
+				while ((message = reader.readLine()) != null) {
+					System.out.println("read “ + message");
+					incoming.append(message + "\n");
+				} // close while
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		} // close run
 	}
 
 }
